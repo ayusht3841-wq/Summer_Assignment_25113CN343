f128881148a7717c470e217bf5154e788ccc4721
ayusht3841-wq <ayusht3841@gmail.com>
2026-05-30 22:07:33 +0530

    initial commit

---
+++ Q10.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+    int start,end,count;
+
+    printf("Enter the starting number: ");
+    scanf("%d",&start);
+
+    printf("Enter ending number: ");
+    scanf("%d",&end);
+
+    for(int i=start;i<=end;i++)
+    {
+        count=0;
+        for(int j=1;j<=i;j++)
+        {
+            if(i%j==0)
+            {
+                count++;
+            }
+        }
+        if(count==2)
+        printf("%d ",i);
+    }
+
+    return 0;
+}
