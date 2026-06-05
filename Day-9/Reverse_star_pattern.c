8e304af0ae136c7c7463a6a6c9da560e7cc64866
ayusht3841-wq <ayusht3841@gmail.com>
2026-06-05 14:17:46 +0530

    Day-9 assignment is uploaded

---
+++ Day-9/Reverse_star_pattern.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    for(int i=0;i<5;i++)
+    {
+        for(int j=0;j<5;j++)
+        {
+            if(i+j>=5)
+            {
+                printf(" ");
+            }
+
+            else
+            {
+                printf("*");
+            }
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
